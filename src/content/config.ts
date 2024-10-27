56186d51b6185f30a7e2e944042c83e560af4449
silverblade34 <mpacheco.tacay@gmail.com>
2024-10-26 20:17:05 -0500

    Change: link dinamico

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,10 +5,14 @@ const projects = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string().min(1),
-      link: z.string(),
+      link: z.object({
+        name: z.string().min(1),
+        icon: z.string().optional(),
+        value: z.string(),
+      }),
       description: z.string().min(1),
       image: image(),
-      url: z.string().url(),
+      url: z.string(),
       featured: z.number().min(1).optional(),
     }),
 });
